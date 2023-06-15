c35d51baa7771a4c6576725747d590af8126e00b
LeMoiAK <136517039+LeMoiAK@users.noreply.github.com>
2023-06-15 23:11:05 +0100

    Added file for ActivityImporter module

---
+++ Utilities/ActivityImporter.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+ActivityImporter class
+Class to import and process an activity .fit file
+It also generates metrics and more avanced metrics
+
+Created on Thu Jun 15 22:41:46 2023
+
+@author: LeMoiAK
+"""
+
